2d377c2f465cd4f996e68302f96a8b5f4b9191a9
FatalError <ripcode0@gmail.com>
2025-01-02 19:41:25 +0900

    updated

---
+++ build-system/native-msvc-vscode/engine/engine.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace engine
+{
+    const char* get_name();
+} // namespace engine
---
+++ build-system/native-msvc-vscode/main.cpp
@@ -0,0 +1,14 @@
+#include <engine/engine.h>
+#include <iostream>
+
+int main()
+{
+    int a = 10;
+
+
+    int b = 10;    
+    printf("game name : %s\n", engine::get_name());
+    //system("pause");
+
+    return 0;
+}
